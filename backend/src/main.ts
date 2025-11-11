653475d57f7d0016ee315d5c82d3f559b8ba6dc3
Lasindu Nuwanga Weerasinghe <lasindu_nuwanga_weerasinghe_from.virtusa@vendor.gov.sg>
2025-11-11 14:49:16 +0800

    disable sonar on bootstrap()

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,4 +7,4 @@ async function bootstrap() {
   await app.listen(process.env.PORT ?? 3000);
 }
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
-bootstrap();
+bootstrap(); //NOSONAR
