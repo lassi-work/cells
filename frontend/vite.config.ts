a963d07761af2d29447a6285d2913e62e5cf6eac
Lasindu Nuwanga Weerasinghe <lasindu_nuwanga_weerasinghe_from.virtusa@vendor.gov.sg>
2025-11-12 12:53:53 +0800

    use Number.parseInt

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd());
   return {
     server: {
-      port: env.VITE_PORT ? parseInt(env.VITE_PORT) : 3000,
+      port: env.VITE_PORT ? Number.parseInt(env.VITE_PORT) : 3000,
     },
   };
 });
